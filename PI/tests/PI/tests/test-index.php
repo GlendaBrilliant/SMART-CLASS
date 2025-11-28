282b337dec05547ce0815a639899823874c3d868
FelipeRiseUp <fdemouracaitano@gmail.com>
2025-11-28 16:44:01 -0300

    Update test-index.php

--- PI/tests/PI/tests/test-index.php
+++ PI/tests/PI/tests/test-index.php
@@ -1,1 +1,11 @@
+<?php
+// Teste 1: verificar se index.php existe
+$arquivo = __DIR__ . '/../index.php';
 
+if (file_exists($arquivo)) {
+    echo "TESTE 1 OK - index.php encontrado\n";
+    exit(0);
+} else {
+    echo "TESTE 1 FALHOU - index.php NÃO encontrado\n";
+    exit(1);
+}
